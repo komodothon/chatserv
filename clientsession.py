892a3a6e43f154d3feff5f8ad1acab7ec0096002
Kadir Vedachalam <kadir.v@gmail.com>
2025-06-16 17:51:25 +0530

    online_users stage 2

--- clientsession.py
+++ clientsession.py
@@ -3,8 +3,10 @@
 from datetime import datetime, timezone
 
 class ClientSession:
-    def __init__(self, websocket, user_id):
+    def __init__(self, websocket, user_id, username):
         self.websocket = websocket
         self.user_id = user_id
+        self.username = username 
         self.joined_at = datetime.now(timezone.utc)
         self.left_at = None
+        self.room = None
